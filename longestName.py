b1c659600e17bc65086a25e651d2173dc36d2a46
Rutuja Kadam <86920670+rutu1026@users.noreply.github.com>
2024-03-21 22:12:33 +0530

    Add files via upload

---
+++ longestName.py
@@ -0,0 +1,14 @@
+# class Solution:
+#     def longest(self, names, n):
+#     	# code here
+    	
+#     	maxi = 0
+#     	ind=0
+    	
+#     	for i in range(0,n):
+#     	     l = len(names[i])
+#     	     if l>maxi:
+#     	         maxi = l
+#     	         ind = i
+    	         
+#     	return names[ind]
